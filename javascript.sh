e0a7c5aa3083b4df1649feb76dfd79acac70bb4d
Lewis Goddard <lewisgoddard@eustasy.org>
2016-08-05 22:52:30 +0100

    Update tests

---
+++ javascript.sh
@@ -0,0 +1,18 @@
+
+#!/bin/sh
+# Lints all javascript files with standard
+# Requires nvm installed
+
+set -e
+
+echo "###########################"
+echo "Starting Javascript linting"
+echo "###########################"
+
+npm install standard
+
+./node_modules/.bin/standard --env *.js
+
+echo "############################"
+echo "Javascript linting complete!"
+echo "############################"
--- php.sh
+++ php.sh
@@ -1,7 +1,19 @@
-find ./ -name '*.php' -exec php -l {} \;
+#!/bin/sh
+# Runs php -l on all php files
 
-if find ./ -name '*.php' -exec php -l {} \; |grep -v 'No syntax error'
+set -e
+
+echo "###################"
+echo "Startin PHP linting"
+echo "###################"
+
+find . -name "*.php" -print0 | xargs -0 -n1 -P8 php -l
+if find . -name "*.php" -print0 | xargs -0 -n1 -P8 php -l | grep -v 'No syntax error'
 then
-	echo 'There are PHP errors.'
-	exit 1;
+        echo 'There are PHP errors.'
+        exit 1;
 fi
+
+echo "#####################"
+echo "PHP linting complete!"
+echo "#####################"
