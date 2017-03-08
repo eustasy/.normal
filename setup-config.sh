6f6903141349755f8c3f6f8e25453aa83a5e338e
Lewis Goddard <lewisgoddard@eustasy.org>
2017-03-08 17:02:51 +0100

    Update setup-config.sh

--- setup-config.sh
+++ setup-config.sh
@@ -1,3 +1,29 @@
-cp .stylelintrc.json ../.stylelintrc.json
-cp .stylelintignore  ../.stylelintignore
-cp .eslint.json      ../.eslint.json
+#!/bin/sh
+# Copy config files
+
+set -e
+
+echo "####################"
+echo "Starting Config Copy"
+echo "####################"
+
+cp normal-checks/.stylelintrc.json .stylelintrc.json
+cp normal-checks/.stylelintignore  .stylelintignore
+cp normal-checks/.eslint.json      .eslint.json
+
+if ! git --no-pager diff --quiet; then
+    git --no-pager diff
+
+    echo "########################"
+    echo "Config changes detected."
+    echo "########################"
+
+    exit 1;
+else
+    echo "#####################"
+    echo "Config copy complete!"
+    echo "#####################"
+
+    exit 0;
+fi
+
