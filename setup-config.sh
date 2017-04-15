e7ea40a0472d02fa4d9c565797ccb90a36e97d95
Lewis Goddard <lewisgoddard@eustasy.org>
2017-04-15 19:26:07 +0100

    Update setup-config.sh

--- setup-config.sh
+++ setup-config.sh
@@ -10,6 +10,8 @@ echo "####################"
 cp normal-checks/.codeclimate.yml  .codeclimate.yml
 cp normal-checks/.eslintrc.json    .eslintrc.json
 cp normal-checks/.eslintignore     .eslintignore
+cp normal-checks/.mdlrc            .mdlrc
+cp normal-checks/.mdlrc.style.rb   .mdlrc.style.rb
 cp normal-checks/.stylelintrc.json .stylelintrc.json
 cp normal-checks/.stylelintignore  .stylelintignore
 
