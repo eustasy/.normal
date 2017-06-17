5e7fd2eae079ae6880c3a36673f5f197be347db2
Lewis Goddard <lewisgoddard@eustasy.org>
2017-06-17 18:47:51 +0100

    Update check-javascript.sh

--- check-javascript.sh
+++ check-javascript.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-# Lints all javascript files with standard
-# Requires nvm installed
+# Syntax check JavaScript with Acorn
+# https://github.com/ternjs/acorn
 
 set -e
 
-echo "###########################"
-echo "Starting Javascript linting"
-echo "###########################"
+echo "################################"
+echo "Starting Javascript syntax check"
+echo "################################"
 
-npm install --silent -g eslint-config-standard eslint-plugin-standard eslint
+npm install --silent -g acorn
 
-eslint "*.js"
+acorn --silent "*.js"; echo $? 
 
-echo "############################"
-echo "Javascript linting complete!"
-echo "############################"
+echo "#################################"
+echo "Javascript syntax check complete!"
+echo "#################################"
