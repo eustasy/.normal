93f62a9f7f1f46a34322bc5ee6d779af94f5131f
Lewis Goddard <lewisgoddard@eustasy.org>
2017-06-17 18:58:31 +0100

    Update check-javascript.sh

--- check-javascript.sh
+++ check-javascript.sh
@@ -3,15 +3,13 @@
 # https://github.com/ternjs/acorn
 # inspired by https://stackoverflow.com/a/24385950/1239965
 
-set -e
-
 echo "################################"
 echo "Starting Javascript syntax check"
 echo "################################"
 
 npm install --silent -g acorn
 
-acorn --silent "**/*.js"; echo $? 
+find . -name '*.js' | xargs acorn --silent; echo $? 
 
 echo "#################################"
 echo "Javascript syntax check complete!"
