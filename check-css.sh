9c4d1188982ed75fcb26dee6bccf5f6b275ed6e0
Lewis Goddard <lewisgoddard@eustasy.org>
2018-03-28 18:39:46 +0100

    Update check-css.sh

--- check-css.sh
+++ check-css.sh
@@ -4,23 +4,23 @@
 
 set -e
 
-echo "####################"
-echo "Starting CSS linting"
-echo "####################"
+echo "###################"
+echo "Starting CSS checks"
+echo "###################"
 
-npm install --silent -g stylelint stylelint-order
-stylelint --config .stylelintrc.json "**/*.css"
+npm install --silent -g stylelint stylelint-order stylelint-csstree-validator
+stylelint --config .stylelintrc.syntax.json "**/*.css"
 
 if ! git --no-pager diff --quiet; then
     git --no-pager diff
-    echo "##############################"
-    echo "CSS linting detected an error."
-    echo "##############################"
+    echo "#############################"
+    echo "CSS checks detected an error."
+    echo "#############################"
     exit 1;
 
 else
-    echo "#####################"
-    echo "CSS linting complete!"
-    echo "#####################"
+    echo "####################"
+    echo "CSS checks complete!"
+    echo "####################"
     exit 0;
 fi
