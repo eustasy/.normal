df1248f11adcb45dcb177711d4d153c6a0b327ff
Lewis Goddard <lewisgoddard@eustasy.org>
2020-04-24 14:52:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
-# Install
 
-set -e
-
-find . -type d -exec sudo chmod 755 {} \;
-find . -type f -exec sudo chmod 644 {} \;
-find . -type f -name "*.sh" -exec sudo chmod 755 {} \;
-
-cp normal-checks/.codacy.yml       .codacy.yml
 cp normal-checks/.codeclimate.yml  .codeclimate.yml
 cp normal-checks/.eslintrc.json    .eslintrc.json
 cp normal-checks/.eslintignore     .eslintignore
