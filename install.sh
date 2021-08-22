56b536acc325ad7fd171354db608c365304d44db
Lewis Goddard <lewisgoddard@eustasy.org>
2021-08-22 20:34:07 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ rm .codacy.yml
 rm check-config.sh
 rm check-permissions.sh
 
-# SCopy current dot files
-cp normal-checks/.codeclimate.yml  .codeclimate.yml
-cp normal-checks/.eslintrc.json    .eslintrc.json
-cp normal-checks/.eslintignore     .eslintignore
-cp normal-checks/.mdlrc            .mdlrc
-cp normal-checks/.mdlrc.style.rb   .mdlrc.style.rb
-cp normal-checks/.stylelintrc.json .stylelintrc.json
-cp normal-checks/.stylelintignore  .stylelintignore
-cp normal-checks/.travis.yml       .travis.yml
+# Copy current dot files
+cp .normal/.codeclimate.yml  .codeclimate.yml
+cp .normal/.eslintrc.json    .eslintrc.json
+cp .normal/.eslintignore     .eslintignore
+cp .normal/.mdlrc            .mdlrc
+cp .normal/.mdlrc.style.rb   .mdlrc.style.rb
+cp .normal/.stylelintrc.json .stylelintrc.json
+cp .normal/.stylelintignore  .stylelintignore
+cp .normal/.travis.yml       .travis.yml
