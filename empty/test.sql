358268324daed1751beddee7276e9bdc9b670f61
Lewis Goddard <lewis.goddard@eustasy.org>
2026-06-02 17:06:18 +0100

    Update test.sql

--- empty/test.sql
+++ empty/test.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `Runonces` (
-  `Runonce` varchar(128) NOT NULL,
-  `Session` varchar(128) NOT NULL,
-  `Active` int(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+      `Runonce` varchar(128) NOT NULL,
+      `Session` varchar(128) NOT NULL,
+      `Active` int(1) NOT NULL DEFAULT '1'
+  ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
